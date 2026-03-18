d29953b4e6749905b730de438c00781baf2c48de
dipodidae <senseosoft@gmail.com>
2026-03-18 12:54:01 +0100

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,13 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/eslint',
   ],
+
   devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
   compatibilityDate: '2024-04-03',
+
   eslint: {
     config: {
       standalone: false,
